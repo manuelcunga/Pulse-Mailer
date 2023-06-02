0deaf7534f74dcdba1350599b41468641b16128b
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-06-02 20:25:05 +0100

    feat: :sparkles:  add create new campaign usecase

--- usecases/campaign/createCampaignUsecase.go
+++ usecases/campaign/createCampaignUsecase.go
@@ -1,7 +1,6 @@
 package campaign_usecase
 
 import (
-	internal_errors "github.com/manuelcunga/Pulse-Mailer/api/rest/errors"
 	entities "github.com/manuelcunga/Pulse-Mailer/domain/entities/campaign"
 	"github.com/manuelcunga/Pulse-Mailer/domain/repository"
 	"github.com/manuelcunga/Pulse-Mailer/usecases/campaign/dtos"
@@ -11,17 +10,17 @@ type CampaignUsecase interface {
 	Create(campaign *dtos.CreateCampaignInput) (string, error)
 }
 
-type CampaignUseCaseRepo struct {
+type CampaignUseCaseImplment struct {
 	CampaignRepo repository.IcampaignRepository
 }
 
 func NewCampaignUseCase(campaignRepo repository.IcampaignRepository) CampaignUsecase {
-	return &CampaignUseCaseRepo{
+	return &CampaignUseCaseImplment{
 		CampaignRepo: campaignRepo,
 	}
 }
 
-func (cp CampaignUseCaseRepo) Create(data *dtos.CreateCampaignInput) (string, error) {
+func (cp CampaignUseCaseImplment) Create(data *dtos.CreateCampaignInput) (string, error) {
 	campaign, err := entities.NewCampaign(data.Name, data.Content, data.Contacts)
 
 	if err != nil {
@@ -31,7 +30,7 @@ func (cp CampaignUseCaseRepo) Create(data *dtos.CreateCampaignInput) (string, er
 	err = cp.CampaignRepo.Create(campaign)
 
 	if err != nil {
-		return "", internal_errors.ErrInternal
+		return "", err
 	}
 	return campaign.ID, nil
 
