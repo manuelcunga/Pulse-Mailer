bc557307505118ff562e2f10ef27bc0a7bcf6da1
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-06-02 20:23:01 +0100

    chore: :recycle: refactor repository interface

--- domain/repository/IcampaignRepository.go
+++ domain/repository/IcampaignRepository.go
@@ -1,9 +1,11 @@
 package repository
 
-import (
-	entities "github.com/manuelcunga/Pulse-Mailer/domain/entities/campaign"
-)
+import entities "github.com/manuelcunga/Pulse-Mailer/domain/entities/campaign"
 
 type IcampaignRepository interface {
 	Create(campaign *entities.Campaign) error
+	Update(campaign *entities.Campaign) error
+	FindAll() ([]*entities.Campaign, error)
+	GetByID(id string) (*entities.Campaign, error)
+	Delete(id string) error
 }
